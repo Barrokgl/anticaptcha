f910d6a528357d60568e21747cd58ae8359fd9ae
Barrokgl <barrokgl@gmail.com>
2017-06-29 02:26:14 +0300

    debuging api response

--- anticaptcha.go
+++ anticaptcha.go
@@ -7,9 +7,13 @@ import (
 	"net/http"
 	"net/url"
 	"time"
+	"io/ioutil"
+	"os"
+	"math"
 )
 
 var (
+	logger = log.New(os.Stdout, "anti: ", log.LstdFlags)
 	baseURL      = &url.URL{Host: "api.anti-captcha.com", Scheme: "https", Path: "/"}
 	sendInterval = 10 * time.Second
 	userAgent    = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36"
@@ -50,22 +54,33 @@ func (c *Client) createTaskRecaptcha(websiteURL, recaptchaKey string) float64 {
 
 	b, err := json.Marshal(body)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	// Make the request
 	u := baseURL.ResolveReference(&url.URL{Path: "/createTask"})
 	resp, err := http.Post(u.String(), "application/json", bytes.NewBuffer(b))
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer resp.Body.Close()
 
 	// Decode response
 	responseBody := make(map[string]interface{})
 	json.NewDecoder(resp.Body).Decode(&responseBody)
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	logger.Println(string(response))
+	
 	// TODO treat api errors and handle them properly
-	return responseBody["taskId"].(float64)
+	val, ok := responseBody["taskId"]
+	if !ok {
+		return float64(math.NaN())
+	}
+	return val.(float64)
 }
 
 // Method to check the result of a given task, returns the json returned from the api
@@ -77,20 +92,27 @@ func (c *Client) getTaskResult(taskID float64) map[string]interface{} {
 	}
 	b, err := json.Marshal(body)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	// Make the request
 	u := baseURL.ResolveReference(&url.URL{Path: "/getTaskResult"})
 	resp, err := http.Post(u.String(), "application/json", bytes.NewBuffer(b))
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer resp.Body.Close()
 
 	// Decode response
 	responseBody := make(map[string]interface{})
 	json.NewDecoder(resp.Body).Decode(&responseBody)
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	logger.Println(string(response))
+
 	return responseBody
 }
 
@@ -105,11 +127,11 @@ func (c *Client) SendRecaptcha(websiteURL string, recaptchaKey string) string {
 	response := c.getTaskResult(taskID)
 	for {
 		if response["status"] == "processing" {
-			log.Println("Result is not ready, waiting a few seconds to check again...")
+			logger.Println("Result is not ready, waiting a few seconds to check again...")
 			time.Sleep(sendInterval)
 			response = c.getTaskResult(taskID)
 		} else {
-			log.Println("Result is ready.")
+			logger.Println("Result is ready.")
 			break
 		}
 	}
@@ -129,14 +151,14 @@ func (c *Client) createTaskImage(imgString string) float64 {
 
 	b, err := json.Marshal(body)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	// Make the request
 	u := baseURL.ResolveReference(&url.URL{Path: "/createTask"})
 	resp, err := http.Post(u.String(), "application/json", bytes.NewBuffer(b))
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer resp.Body.Close()
 
@@ -158,11 +180,11 @@ func (c *Client) SendImage(imgString string) string {
 	response := c.getTaskResult(taskID)
 	for {
 		if response["status"] == "processing" {
-			log.Println("Result is not ready, waiting a few seconds to check again...")
+			logger.Println("Result is not ready, waiting a few seconds to check again...")
 			time.Sleep(sendInterval)
 			response = c.getTaskResult(taskID)
 		} else {
-			log.Println("Result is ready.")
+			logger.Println("Result is ready.")
 			break
 		}
 	}
